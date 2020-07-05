9c0b20b484dfe0e3f3903961f62cc653f899c555
sairamreddye <esairamreddy@gmail.com>
2020-07-05 10:33:15 +0530

    commiting the code for calling local json file

--- src/app/components/smartComponents/items-state/items-state.component.ts
+++ src/app/components/smartComponents/items-state/items-state.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemsDataService } from '../../../services/itemsData/items-data.service';
 import { catchError,map } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { throwError, Observable } from 'rxjs';
 import { of } from 'rxjs';
-
+import data from '../../../data/shoping.json';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -13,26 +14,35 @@ import { of } from 'rxjs';
 })
 export class ItemsStateComponent implements OnInit {
   items: any[];
-    
-  constructor(private itemsData:ItemsDataService) { }
+  private _jsonURL = '../../../data/shoping.json';
+  constructor(private itemsData:ItemsDataService,private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+      console.log(data);
+     });
+   }
 
+   public getJSON(): Observable<any> {
+    return this.http.get(this._jsonURL);
+  }
+  
   ngOnInit() {
-    this.itemsData.getItems().pipe(
-      map((res:any []) => this.items = res),
-      catchError(err => {
-          console.log('caught mapping error and rethrowing', err);
-          return throwError(err);
-      }),
-      catchError(err => {
-          console.log('caught rethrown error, providing fallback value',err);
-          return of([]);
-      })
-  )
-  .subscribe(
-      res => console.log('HTTP response',res),
-      err => console.log('HTTP Error', err),
-      () => console.log('HTTP request completed.')
-  );
+  //   this.itemsData.getItems().pipe(
+  //     map((res:any []) => this.items = res),
+  //     catchError(err => {
+  //         console.log('caught mapping error and rethrowing', err);
+  //         return throwError(err);
+  //     }),
+  //     catchError(err => {
+  //         console.log('caught rethrown error, providing fallback value',err);
+  //         return of([]);
+  //     })
+  // )
+  // .subscribe(
+  //     res => console.log('HTTP response',res),
+  //     err => console.log('HTTP Error', err),
+  //     () => console.log('HTTP request completed.')
+  // );
+  this.items = data;
   }
 
 }
